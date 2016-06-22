2872e0ef34c32efa4db6ae746a005c3618980f23
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2016-06-22 13:18:56 +0200

    Simplify some code
    * Create functions to do the uint <-> byte[] automatically
    * Add some loops

--- BlowFishCipher.cs
+++ BlowFishCipher.cs
@@ -19,15 +19,17 @@ namespace hmailserver_password
 
         void Initialize(byte[] key)
         {
+            // Double the key to be able to take slices of 4 at all times
+            var doubleKey = new byte[key.Length * 2];
+            Array.Copy(key, 0, doubleKey, 0, key.Length);
+            Array.Copy(key, 0, doubleKey, key.Length, key.Length);
+
             Constants.bf_P.CopyTo(PArray, 0);
             Constants.bf_S.CopyTo(SBoxes, 0);
+
             for (int i = 0, j = 0; i < Constants.NPASS + 2; ++i, j += 4)
             {
-                uint temp = 
-                    (uint)key[(j + 0) % key.Length] << 24
-                    | (uint)key[(j + 1) % key.Length] << 16
-                    | (uint)key[(j + 2) % key.Length] << 8
-                    | (uint)key[(j + 3) % key.Length] << 0;
+                uint temp = UIntFromBytes(doubleKey, j % key.Length, false);
                 PArray[i] ^= temp;
             }
 
@@ -39,7 +41,7 @@ namespace hmailserver_password
                 PArray[i] = xlxr.Item1;
                 PArray[i + 1] = xlxr.Item2;
             }
-            
+
             for (int i = 0; i < 4; ++i)
             {
                 for (int j = 0; j < 256; j += 2)
@@ -57,10 +59,11 @@ namespace hmailserver_password
             //#define bf_F(x) (((S(x,0) + S(x,1)) ^ S(x,2)) + S(x,3))
             //#define ROUND(a,b,n) (a.dword ^= bf_F(b) ^ PArray[n])
 
-            uint S0 = SBoxes[0 * 256 + ((b >> 24) & 0xFF)];
-            uint S1 = SBoxes[1 * 256 + ((b >> 16) & 0xFF)];
-            uint S2 = SBoxes[2 * 256 + ((b >> 8) & 0xFF)];
-            uint S3 = SBoxes[3 * 256 + ((b >> 0) & 0xFF)];
+            var sbytes = BytesFromUInt(b, false);
+            uint S0 = SBoxes[0 * 256 + sbytes[0]];
+            uint S1 = SBoxes[1 * 256 + sbytes[1]];
+            uint S2 = SBoxes[2 * 256 + sbytes[2]];
+            uint S3 = SBoxes[3 * 256 + sbytes[3]];
             uint bf_F = ((S0 + S1) ^ S2) + S3;
             return bf_F ^ PArray[n];
         }
@@ -71,22 +74,10 @@ namespace hmailserver_password
             uint xr = xlxr.Item2;
 
             xl ^= PArray[0];
-            xr ^= Round(xl, 1);
-            xl ^= Round(xr, 2);
-            xr ^= Round(xl, 3);
-            xl ^= Round(xr, 4);
-            xr ^= Round(xl, 5);
-            xl ^= Round(xr, 6);
-            xr ^= Round(xl, 7);
-            xl ^= Round(xr, 8);
-            xr ^= Round(xl, 9);
-            xl ^= Round(xr, 10);
-            xr ^= Round(xl, 11);
-            xl ^= Round(xr, 12);
-            xr ^= Round(xl, 13);
-            xl ^= Round(xr, 14);
-            xr ^= Round(xl, 15);
-            xl ^= Round(xr, 16);
+            for (int i = 1; i < 17; i += 2) {
+                xr ^= Round(xl, i);
+                xl ^= Round(xr, i + 1);
+            }
             xr ^= PArray[17];
 
             return Tuple.Create(xr, xl);
@@ -98,22 +89,10 @@ namespace hmailserver_password
             uint xr = xlxr.Item2;
 
             xl ^= PArray[17];
-            xr ^= Round(xl, 16);
-            xl ^= Round(xr, 15);
-            xr ^= Round(xl, 14);
-            xl ^= Round(xr, 13);
-            xr ^= Round(xl, 12);
-            xl ^= Round(xr, 11);
-            xr ^= Round(xl, 10);
-            xl ^= Round(xr, 9);
-            xr ^= Round(xl, 8);
-            xl ^= Round(xr, 7);
-            xr ^= Round(xl, 6);
-            xl ^= Round(xr, 5);
-            xr ^= Round(xl, 4);
-            xl ^= Round(xr, 3);
-            xr ^= Round(xl, 2);
-            xl ^= Round(xr, 1);
+            for (int i = 16; i > 0; i -= 2) {
+                xr ^= Round(xl, i);
+                xl ^= Round(xr, i - 1);
+            }
             xr ^= PArray[0];
 
             return Tuple.Create(xr, xl);
@@ -142,30 +121,20 @@ namespace hmailserver_password
             byte[] output = new byte[input.Length];
             for (int i = 0; i < input.Length; i += 8)
             {
-                uint xl =
-                    ((uint)input[i + 3] << 24) |
-                    ((uint)input[i + 2] << 16) |
-                    ((uint)input[i + 1] << 8) |
-                    ((uint)input[i + 0] << 0);
-                uint xr =
-                    ((uint)input[i + 7] << 24) |
-                    ((uint)input[i + 6] << 16) |
-                    ((uint)input[i + 5] << 8) |
-                    ((uint)input[i + 4] << 0);
+                uint xl = UIntFromBytes(input, i, true);
+                uint xr = UIntFromBytes(input, i + 4, true);
 
                 Tuple<uint, uint> xlxr = Tuple.Create(xl, xr);
                 xlxr = Decipher(xlxr);
                 xl = xlxr.Item1;
                 xr = xlxr.Item2;
 
-                output[i + 3] = (byte)((xl >> 24) & 0xFF);
-                output[i + 2] = (byte)((xl >> 16) & 0xFF);
-                output[i + 1] = (byte)((xl >> 8) & 0xFF);
-                output[i + 0] = (byte)((xl >> 0) & 0xFF);
-                output[i + 7] = (byte)((xr >> 24) & 0xFF);
-                output[i + 6] = (byte)((xr >> 16) & 0xFF);
-                output[i + 5] = (byte)((xr >> 8) & 0xFF);
-                output[i + 4] = (byte)((xr >> 0) & 0xFF);
+                var outputl = BytesFromUInt(xl, true);
+                var outputr = BytesFromUInt(xr, true);
+                for (int j = 0; j < 4; ++j) {
+                    output[i + j] = outputl[j];
+                    output[i + j + 4] = outputr[j];
+                }
             }
 
             return output;
@@ -181,30 +150,20 @@ namespace hmailserver_password
             byte[] output = new byte[input.Length];
             for (int i = 0; i < input.Length; i += 8)
             {
-                uint xl =
-                    ((uint)input[i + 3] << 24) |
-                    ((uint)input[i + 2] << 16) |
-                    ((uint)input[i + 1] << 8) |
-                    ((uint)input[i + 0] << 0);
-                uint xr =
-                    ((uint)input[i + 7] << 24) |
-                    ((uint)input[i + 6] << 16) |
-                    ((uint)input[i + 5] << 8) |
-                    ((uint)input[i + 4] << 0);
+                uint xl = UIntFromBytes(input, i, true);
+                uint xr = UIntFromBytes(input, i + 4, true);
 
                 Tuple<uint, uint> xlxr = Tuple.Create(xl, xr);
                 xlxr = Encipher(xlxr);
                 xl = xlxr.Item1;
                 xr = xlxr.Item2;
 
-                output[i + 3] = (byte)((xl >> 24) & 0xFF);
-                output[i + 2] = (byte)((xl >> 16) & 0xFF);
-                output[i + 1] = (byte)((xl >> 8) & 0xFF);
-                output[i + 0] = (byte)((xl >> 0) & 0xFF);
-                output[i + 7] = (byte)((xr >> 24) & 0xFF);
-                output[i + 6] = (byte)((xr >> 16) & 0xFF);
-                output[i + 5] = (byte)((xr >> 8) & 0xFF);
-                output[i + 4] = (byte)((xr >> 0) & 0xFF);
+                var outputl = BytesFromUInt(xl, true);
+                var outputr = BytesFromUInt(xr, true);
+                for (int j = 0; j < 4; ++j) {
+                    output[i + j] = outputl[j];
+                    output[i + j + 4] = outputr[j];
+                }
             }
 
             return output;
@@ -213,18 +172,39 @@ namespace hmailserver_password
         public static byte[] FromHex(string input)
         {
             try {
-            var bytes = new byte[input.Length / 2];
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                string part = input.Substring(i * 2, 2);
-                bytes[i] = Convert.ToByte(part, 16);
-            }
-            return bytes;
+                var bytes = new byte[input.Length / 2];
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    string part = input.Substring(i * 2, 2);
+                    bytes[i] = Convert.ToByte(part, 16);
+                }
+                return bytes;
             }
             catch (FormatException e)
             {
                 throw new ArgumentException(e.Message);
             }
+        }
+
+        static uint UIntFromBytes(byte[] input, int offset, bool littleEndian) {
+            var output = new byte[4];
+            Array.Copy(input, offset, output, 0, 4);
+
+            if (BitConverter.IsLittleEndian != littleEndian) {
+                Array.Reverse(output);
+            }
+
+            return BitConverter.ToUInt32(output, 0);
+        }
+
+        static byte[] BytesFromUInt(uint input, bool littleEndian) {
+            var bytes = BitConverter.GetBytes(input);
+
+            if (BitConverter.IsLittleEndian != littleEndian) {
+                Array.Reverse(bytes);
+            }
+
+            return bytes;
         }
 
         public static string ToHex(byte[] input)
